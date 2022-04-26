8855652cd22afbbf2f91aed59a99f633b0422320
ASU Data Science <asudatascience@datasciencelab.3500.dhcp.asu.edu>
2022-04-25 19:12:03 -0700

    Client Side

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 let success = true
 const submit = document.getElementById("submit")
 
-if (submit) {
-    console.log('chandan')
-    submit.addEventListener("click", (e) => {
-        console.log('chandan')
-        // console.log(e)
-        // console.log(alert_success)
-        // console.log(alert_danger)
-    })
-}
-
-function submitButton(){
-    // debugger
+function submitButton() {
     let alert_success = document.querySelector(".alert-success")
     let alert_danger = document.querySelector(".alert-danger")
-    
-    if(success){
+    const input = document.getElementsByClassName('form-control');
+
+    for (let index = 0; index < input.length; index++) {
+        const element = input[index];
+        console.log(element.value)
+    }
+
+    if (success) {
         alert_success.hidden = false
     }
-    else{
+    else {
         alert_danger.hidden = false
     }
 }
