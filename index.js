e45dcd946ce893f40eba379aab1f429e5ff190d4
Chandan Gowda Nandakumar <chandangowdanandakumar@chandans-mbp.3500.dhcp.asu.edu>
2022-04-26 13:58:54 -0700

    Number of Employees

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let success = true
+// let success = true
 const submit = document.getElementById("submit")
 
 function submitButton() {
     let alert_success = document.querySelector(".alert-success")
     let alert_danger = document.querySelector(".alert-danger")
+    let NoOfEmp = document.querySelector('#NumEmp')
     const input = document.getElementsByClassName('form-control');
 
     for (let index = 0; index < input.length; index++) {
@@ -11,10 +12,13 @@ function submitButton() {
         console.log(element.value)
     }
 
-    if (success) {
+    if (NoOfEmp.value > 500) {
         alert_success.hidden = false
+        alert_danger.hidden = true
     }
     else {
+        alert_success.hidden = true
         alert_danger.hidden = false
+        
     }
 }
